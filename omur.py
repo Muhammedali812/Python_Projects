6f608ea70129da75390e5ca537d9b6d1cccd16f2
Muhammedali812 <muhmmdlir@gmail.com>
2024-10-02 05:59:22 +0400

    omur.py

---
+++ omur.py
@@ -0,0 +1,12 @@
+
+yas=int(input('yas: '))
+if yas<=12:
+	print('usaq')
+elif yas>12 and yas<=17:
+	print('yeniyetme')
+elif yas>17 and yas<=30:
+	print('cavan')
+elif yas>30 and yas<=50:
+	print('orta')
+elif yas>50:
+	print('yaşlı')
